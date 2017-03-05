789e5e93799f45c925a404670862489035dd415b
renkastle <rene.castillo@outlook.com>
2017-03-05 17:48:07 -0600

    Subir Proyecto a Git

---
+++ TareaU/TareaU/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace TareaU
+{
+    partial class frmConvert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConvert));
+            this.cmbOrigen = new System.Windows.Forms.ComboBox();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.lblDestino = new System.Windows.Forms.Label();
+            this.cmbDestino = new System.Windows.Forms.ComboBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblRespuesta = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbOrigen
+            // 
+            this.cmbOrigen.FormattingEnabled = true;
+            this.cmbOrigen.Items.AddRange(new object[] {
+            "--Unidades de Masa--",
+            "Tonelada",
+            "Kilogramo",
+            "Gramo",
+            "Miligramo",
+            "Libra",
+            "Onza",
+            "--Unidades de Longitud--",
+            "Metro",
+            "Yarda",
+            "Vara",
+            "Milla",
+            "Kilometro",
+            "Pie",
+            "Pulgada",
+            "--Unidades de Temperatura--",
+            "Centigrados",
+            "Fahrenheit",
+            "Kelvin",
+            "--Unidades de Tiempo--",
+            "Horas",
+            "Minutos",
+            "Segundos",
+            "Dia"});
+            this.cmbOrigen.Location = new System.Drawing.Point(13, 133);
+            this.cmbOrigen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbOrigen.Name = "cmbOrigen";
+            this.cmbOrigen.Size = new System.Drawing.Size(180, 28);
+            this.cmbOrigen.TabIndex = 0;
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(9, 108);
+            this.lblOrigen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(133, 20);
+            this.lblOrigen.TabIndex = 1;
+            this.lblOrigen.Text = "Unidad de Origen";
+            // 
+            // lblDestino
+            // 
+            this.lblDestino.AutoSize = true;
+            this.lblDestino.Location = new System.Drawing.Point(260, 108);
+            this.lblDestino.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDestino.Name = "lblDestino";
+            this.lblDestino.Size = new System.Drawing.Size(141, 20);
+            this.lblDestino.TabIndex = 2;
+            this.lblDestino.Text = "Unidad de Destino";
+            // 
+            // cmbDestino
+            // 
+            this.cmbDestino.FormattingEnabled = true;
+            this.cmbDestino.Items.AddRange(new object[] {
+            "--Unidades de Masa--",
+            "Tonelada",
+            "Kilogramo",
+            "Gramo",
+            "Miligramo",
+            "Libra",
+            "Onza",
+            "--Unidades de Longitud--",
+            "Metro",
+            "Yarda",
+            "Vara",
+            "Milla",
+            "Kilometro",
+            "Pie",
+            "Pulgada",
+            "--Unidades de Temperatura--",
+            "Centigrados",
+            "Fahrenheit",
+            "Kelvin",
+            "--Unidades de Tiempo--",
+            "Horas",
+            "Minutos",
+            "Segundos",
+            "Dia"});
+            this.cmbDestino.Location = new System.Drawing.Point(264, 133);
+            this.cmbDestino.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbDestino.Name = "cmbDestino";
+            this.cmbDestino.Size = new System.Drawing.Size(180, 28);
+            this.cmbDestino.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(188, 202);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(112, 35);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convertir";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(264, 63);
+            this.txtRespuesta.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(148, 26);
+            this.txtRespuesta.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(13, 63);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(148, 26);
+            this.txtCantidad.TabIndex = 6;
+            this.txtCantidad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCantidad_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cantidad a  Convertir";
+            // 
+            // lblRespuesta
+            // 
+            this.lblRespuesta.AutoSize = true;
+            this.lblRespuesta.Location = new System.Drawing.Point(260, 25);
+            this.lblRespuesta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRespuesta.Name = "lblRespuesta";
+            this.lblRespuesta.Size = new System.Drawing.Size(87, 20);
+            this.lblRespuesta.TabIndex = 8;
+            this.lblRespuesta.Text = "Respuesta";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.cmbOrigen);
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Controls.Add(this.lblRespuesta);
+            this.groupBox1.Controls.Add(this.txtRespuesta);
+            this.groupBox1.Controls.Add(this.lblOrigen);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.cmbDestino);
+            this.groupBox1.Controls.Add(this.lblDestino);
+            this.groupBox1.Location = new System.Drawing.Point(18, 34);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(951, 278);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Convertidor de Unidades";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(452, 25);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(491, 231);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Origen";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Destino";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Resultado";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // frmConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 333);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmConvert";
+            this.Text = "Convertidor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbOrigen;
+        private System.Windows.Forms.Label lblOrigen;
+        private System.Windows.Forms.Label lblDestino;
+        private System.Windows.Forms.ComboBox cmbDestino;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblRespuesta;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ TareaU/TareaU/Form1.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaU
+{
+    public partial class frmConvert : Form
+    {
+        public frmConvert()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+         
+            if (((txtCantidad.Text=="") || (cmbDestino.Text=="") || (cmbOrigen.Text=="")))
+            {
+                MessageBox.Show("Campo vacio");
+            }
+            else if ((((cmbOrigen.Text=="--Unidades de Masa--") || (cmbOrigen.Text=="--Unidades de Longitud--") || (cmbOrigen.Text=="--Unidades de Temperatura--")|| (cmbOrigen.Text=="--Unidades de Tiempo---"))))
+            {
+                 MessageBox.Show("ERROR, Seleccione una Unidad");
+            }
+            else if ((((cmbDestino.Text == "--Unidades de Masa--") || (cmbDestino.Text == "--Unidades de Longitud--") || (cmbDestino.Text == "--Unidades de Temperatura--") || (cmbDestino.Text == "--Unidades de Tiempo---"))))
+            {
+                MessageBox.Show("ERROR, Seleccione una Unidad");
+            }
+            else
+            {
+                int n = dataGridView1.Rows.Add();
+               
+                
+            String from, to;
+
+            from = cmbOrigen.SelectedItem.ToString();
+            to = cmbDestino.SelectedItem.ToString();
+
+            if (from == "Gramo" && to == "Kilogramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.001);
+            }
+            else
+            if (from == "Kilogramo" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1000);
+            }
+            if (from == "Onza" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad) * 28.349);
+            }
+            if (from == "Gramo" && to == "Onza")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad) * 0.035274);
+            }
+            if (from == to)
+            {
+                MessageBox.Show("Has introducido las mismas unidades");
+                txtCantidad.Text = "";
+                txtRespuesta.Text = "";
+                cmbDestino.Text = "";
+                cmbOrigen.Text = "";
+            }
+            ///KILOGRAMO CONTRA TODOS///
+            if (from == "Kilogramo" && to == "Tonelada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.001);
+            }
+            if (from == "Kilogramo" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1000);
+            }
+            if (from == "Kilogramo" && to == "Miligramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1000000);
+            }
+            if (from == "Kilogramo" && to == "Libra")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 2.20462);
+            }
+            if (from == "Kilogramo" && to == "Onza")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 35.274);
+            }
+            ///GRAMO CONTRA TODOS
+            if (from == "Gramo" && to == "Tonelada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000001);
+            }
+            if (from == "Gramo" && to == "Kilogramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.001);
+            }
+            if (from == "Gramo" && to == "Miligramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1000);
+            }
+            if (from == "Gramo" && to == "Libra")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.00220462);
+            }
+            if (from == "Gramo" && to == "Onza")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0352739200000000003);
+            }
+            ///MILIGRAMO CONTRA TODO
+            if (from == "Miligramo" && to == "Tonelada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000000001);
+            }
+            if (from == "Miligramo" && to == "Kilogramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000001);
+            }
+            if (from == "Miligramo" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.001);
+            }
+            if (from == "Miligramo" && to == "Libra")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000002204623);
+            }
+            if (from == "Miligramo" && to == "Onza")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.00003527396);
+            }
+            ///LIBRAS CONTRA TODOS
+            if (from == "Libra" && to == "Tonelada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000453592);
+            }
+            if (from == "Libra" && to == "Kilogramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.453592);
+            }
+            if (from == "Libra" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 453.592);
+            }
+            if (from == "Libra" && to == "Miligramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 453592);
+            }
+            if (from == "Libra" && to == "Onza")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 16);
+            }
+            ///ONZA CONTRA TODOS
+            if (from == "Onza" && to == "Tonelada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.00002834952);
+            }
+            if (from == "Onza" && to == "Kilogramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0283495);
+            }
+            if (from == "Onza" && to == "Gramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 28.3495);
+            }
+            if (from == "Onza" && to == "Miligramo")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 28349.5);
+            }
+            if (from == "Onza" && to == "Libra")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0625);
+            }
+            ///METRO CONTRA TODOS
+            if (from == "Metro" && to == "Yarda")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1.09361);
+            }
+            if (from == "Metro" && to == "Vara")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1.1963081929167);
+            }
+            if (from == "Metro" && to == "Milla")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000621371);
+            }
+            if (from == "Metro" && to == "Kilometro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1000);
+            }
+            if (from == "Metro" && to == "Pie")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 3.28084);
+            }
+            if (from == "Metro" && to == "Pulgada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 39.3701);
+            }
+            ///YARDA CONTRA TODOS
+            if (from == "Yarda" && to == "Metro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.9144);
+            }
+            if (from == "Yarda" && to == "Vara")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1.093904211603);
+            }
+            if (from == "Yarda" && to == "Milla")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000568182);
+            }
+            if (from == "Yarda" && to == "Kilometro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0009144);
+            }
+            if (from == "Yarda" && to == "Pie")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 3);
+            }
+            if (from == "Yarda" && to == "Pulgada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 36);
+            }
+            ///VARA CONTRA TODOS
+            if (from == "Vara" && to == "Metro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.835905);
+            }
+            if (from == "Vara" && to == "Yarda")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.91415682414698);
+            }
+            if (from == "Vara" && to == "Milla")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.00051940728644715);
+            }
+            if (from == "Vara" && to == "Kilometro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000835905);
+            }
+            if (from == "Vara" && to == "Pie")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 2.7424704724409);
+            }
+            if (from == "Vara" && to == "Pulgada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 32.909645669291);
+            }
+            ///MILLA CONTRA TODOS
+            if (from == "Milla" && to == "Metro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1609.344);
+            }
+            if (from == "Milla" && to == "Yarda")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1760);
+            }
+            if (from == "Milla" && to == "Vara")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1925.2714124213);
+            }
+            if (from == "Milla" && to == "Kilometro")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1.609344);
+            }
+            if (from == "Milla" && to == "Pie")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 5280);
+            }
+            if (from == "Milla" && to == "Pulgada")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 63360);
+            }
+
+            ///HORAS  CONTRA TODOS
+            if (from == "Horas" && to == "Minutos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 60);
+            }
+            if (from == "Horas" && to == "Segundos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 3600);
+            }
+            if (from == "Horas" && to == "Dia")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0416667);
+            }
+            ///MINUTOS CONTRA TODOS
+            if (from == "Minutos" && to == "Horas")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0166667);
+            }
+            if (from == "Minutos" && to == "Segundos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 60);
+            }
+            if (from == "Minutos" && to == "Dia")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000694444);
+            }
+            ///SEGUDOS CONTRA TODOS
+            if (from == "Segundos" && to == "Horas")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.000277778);
+            }
+            if (from == "Segundos" && to == "Minutos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0166667);
+            }
+            if (from == "Segundos" && to == "Dia")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 0.0000115741);
+            }
+            ///DIAS CONTRA TODOS
+            if (from == "Dia" && to == "Horas")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 24);
+            }
+            if (from == "Dia" && to == "Minutos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 1440);
+            }
+            if (from == "Dia" && to == "Segundos")
+            {
+                txtRespuesta.Text = Convert.ToString(Convert.ToInt32(txtCantidad.Text) * 86400);
+            }
+            if (from == to)
+            {
+                MessageBox.Show("Has introducido las mismas unidades");
+            }
+            ///
+            dataGridView1.Rows[n].Cells[0].Value = cmbOrigen.SelectedItem.ToString();
+            dataGridView1.Rows[n].Cells[1].Value = cmbDestino.SelectedItem.ToString();
+            dataGridView1.Rows[n].Cells[2].Value = (txtRespuesta.Text);
+
+            txtCantidad.Text = "";
+            txtRespuesta.Text = "";
+            cmbDestino.Text = "";
+            cmbOrigen.Text = "";
+
+            }
+
+        }
+
+       
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCantidad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+                if (Char.IsControl(e.KeyChar)) //permitir teclas de control como retroceso 
+                {
+                    e.Handled = false;
+                }
+                else
+                {
+                    //el resto de teclas pulsadas se desactivan 
+                    e.Handled = true;
+                } 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
